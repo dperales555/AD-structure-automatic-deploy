392fb6d5fec95147e1a9ecd8bf017ad1ec76b317
amosISA <amosisa700@gmail.com>
2020-01-25 09:56:34 +0100

    Añado script de inicio de sesión

---
+++ script_de_inicio_de_sesion.ps1
@@ -0,0 +1,21 @@
+﻿$letterdrive = Get-SmbMapping | Select-Object LocalPath # => Z:
+
+# Función para obtener un string aleatorio
+function randomString() {
+    return [System.Guid]::NewGuid().ToString()
+}
+
+# Comprueba si Z: existe
+if ($letterdrive) {
+    New-Item -Path "$letterdrive\tmp" -ItemType Directory -Force
+}
+
+# Creamos 3 carpetas aleatorias dentro de Z:\tmp
+for ($i=1; $i -le 3; $i++) {
+    New-Item -Type Directory -Path "$letterdrive\tmp\$(randomstring)" -Force
+}
+
+# Mapeamos la unidad G: con GOOGLE_COMPANY
+if(Test-Path \\Serverws2016\google_company) {
+    New-SmbMapping -LocalPath 'G:' -RemotePath '\\Serverws2016\google_company'
+}
