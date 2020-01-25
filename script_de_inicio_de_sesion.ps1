54601234b41c356901f6cd97be129a775597386f
amosISA <amosisa700@gmail.com>
2020-01-25 10:37:44 +0100

    Modificación del script de inicio de sesión

--- script_de_inicio_de_sesion.ps1
+++ script_de_inicio_de_sesion.ps1
@@ -1,4 +1,4 @@
-﻿$letterdrive = Get-SmbMapping | Select-Object LocalPath # => Z:
+﻿$letterdrive = Get-SmbMapping | Where-Object {$_.LocalPath -like 'Z:'} | Select-Object LocalPath # => Z:
 
 # Función para obtener un string aleatorio
 function randomString() {
@@ -7,12 +7,12 @@ function randomString() {
 
 # Comprueba si Z: existe
 if ($letterdrive) {
-    New-Item -Path "$letterdrive\tmp" -ItemType Directory -Force
+    New-Item -Path "Z:\tmp" -ItemType Directory -Force
 }
 
 # Creamos 3 carpetas aleatorias dentro de Z:\tmp
 for ($i=1; $i -le 3; $i++) {
-    New-Item -Type Directory -Path "$letterdrive\tmp\$(randomstring)" -Force
+    New-Item -Type Directory -Path "Z:\tmp\$(randomstring)" -Force
 }
 
 # Mapeamos la unidad G: con GOOGLE_COMPANY
