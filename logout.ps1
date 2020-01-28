508c1a943442ccd03dbe2ed28d077bf38ae64b58
Daniel Perales Mauriz <dperales555@gmail.com>
2020-01-28 16:31:48 +0100

    Comentarios en logout

--- logout.ps1
+++ logout.ps1
@@ -1,6 +1,5 @@
-﻿### ELIMINAMOS TODO EL CONTENIDO DE LA CARPETA Z:\tmp DE CADA USUARIO AL CERRAR SESIÓN ###
-
-# Comprueba si Z: existe
+﻿# Comprueba si Z: existe
 if (Get-SmbMapping | Where-Object {$_.LocalPath -like 'Z:'}) {
+    #Si existse, borra todo elcontenido de la carpeta "tmp"
     Remove-Item Z:\tmp\* -Recurse -Force
 }
